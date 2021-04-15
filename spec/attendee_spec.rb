a05bb282a4888f91bb2072e669f7cce01056dcc5
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-15 08:17:31 -0700

    Create initial set up for attendee spec

---
+++ spec/attendee_spec.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+require './lib/ride'
+require './lib/attendee'
+
+RSpec describe Attendee do
+  context 'initialization' do
+    attendee = Attendee.new('Bob', 20)
+
+    it 'exists' do
+      expect(attendee).to eq(Attendee)
+    end
+  end
+end
+# attendee.name
+# #=> "Bob"
+# attendee.spending_money
+# #=> 20
+# attendee.interests
+# #=> []
+# attendee.add_interest('Bumper Cars')
+# attendee.add_interest('Ferris Wheel')
+# attendee.interests
+# #=> ["Bumper Cars", "Ferris Wheel"]
