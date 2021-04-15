510aae2711989a416e6fdc417800d55b76aa0dc9
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-15 09:17:56 -0700

    Create context block for admittence

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -1,10 +1,12 @@
 class Carnival
   attr_reader :name,
-              :rides
+              :rides,
+              :attendees
 
   def initialize(name)
     @name = name
     @rides = []
+    @attendees = []
   end
 
   def add_ride(ride)
--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe Carnival do
       expect(jeffco_fair.rides).to eq(expected)
     end
 
-    it 'can recommend rides' do
+    xit 'can recommend rides' do
       bob = Attendee.new('Bob', 20)
       sally = Attendee.new('Sally', 20)
 
@@ -47,10 +47,63 @@ RSpec.describe Carnival do
       expected = ['Ferris Wheel', 'Bumper Cars']
 
       expect(jeffco_fair.recommend_rides(bob)).to eq(expected)
+
+      expect(jeffco_fair.recommend_rides(sally)).to eq(['Scrambler'])
+    end
+  end
+
+  context 'admit' do
+    jeffco_fair = Carnival.new("Jefferson County Fair")
+    ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
+    bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
+    scrambler = Ride.new({name: 'Scrambler', cost: 15})
+    jeffco_fair.add_ride(ferris_wheel)
+    jeffco_fair.add_ride(bumper_cars)
+    jeffco_fair.add_ride(scrambler)
+
+    it 'starts with no attendees' do
+      expect(jeffco_fair.attendees).to eq([])
     end
   end
 end
-
-# #=> [#<Ride:0x00007fe59506ede8...>, #<Ride:0x00007fe594851930...>]
-# jeffco_fair.recommend_rides(sally)
-# #=> [#<Ride:0x00007fe59314eb98...>]
+# bob = Attendee.new("Bob", 0)
+# #=> #<Attendee:0x00007ffab70bfce8...>
+# bob.add_interest('Ferris Wheel')
+# bob.add_interest('Bumper Cars')
+# sally = Attendee.new('Sally', 20)
+# #=> #<Attendee:0x00007ffab69069e8...>
+# sally.add_interest('Bumper Cars')
+# johnny = Attendee.new("Johnny", 5)
+# #=> #<Attendee:0x00007ffab7172d70...>
+# johnny.add_interest('Bumper Cars')
+# jeffco_fair.admit(bob)
+# jeffco_fair.admit(sally)
+# jeffco_fair.admit(johnny)
+# jeffco_fair.attendees
+# #=> [#<Attendee:0x00007ffab70bfce8...>, #<Attendee:0x00007ffab69069e8...>, #<Attendee:0x00007ffab7172d70...>]
+#
+# #Attendees are added even if they don't have enough money for all/any exhibits.
+# jeffco_fair.attendees_by_ride_interest
+# #=> {
+# #    #<Ride:0x00007ffab5a3d3a8...> => [#<Attendee:0x00007ffab70bfce8...>],
+# #    #<Ride:0x00007ffab714bce8...> => [#<Attendee:0x00007ffab70bfce8...>, #<Attendee:0x00007ffab69069e8...>, #<Attendee:0x00007ffab7172d70...>],
+# #    #<Ride:0x00007ffab71607d8...> => []
+# #  }
+# jeffco_fair.ticket_lottery_contestants(bumper_cars)
+# #=> [#<Attendee:0x00007ffab70bfce8...>, #<Attendee:0x00007ffab7172d70...>]
+# jeffco_fair.draw_lottery_winner(bumper_cars)
+# #=> "Johnny"
+#
+# #In the above method, with this setup, "Johnny" or "Bob" could be returned here - Fun!
+# jeffco_fair.draw_lottery_winner(ferris_wheel)
+# #=> nil
+#
+# #If no contestants are eligible for the lottery, nil is returned.
+# jeffco_fair.announce_lottery_winner(scrambler)
+# #=> "Bob has won the IMAX exhibit"
+#
+# #We realize that, in this setup, Bob is not interested in the scrambler.  However, in your test, the result should match exactly.  You will need to stub the return of `draw_lottery_winner` as the above method should depend on that method :)
+# jeffco_fair.announce_lottery_winner(ferris_wheel)
+# #=> "No winners for this lottery"
+#
+# #If there are no contestants, there are no winners.
