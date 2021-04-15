1cc588698a2698f1d54e35da98371d162f27a594
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-04-15 09:12:26 -0700

    Start build for recommented_rides, not passing

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -10,4 +10,20 @@ class Carnival
   def add_ride(ride)
     @rides << ride
   end
+
+  def recommend_rides(attendee)
+    ride_names = []
+    @rides.each do |ride|
+      ride_names << ride.name
+    end
+    ride_names
+
+    attendee_interest = []
+    ride_names.each do |ride_name|
+      if ride_name == attendee.interests
+        attendee_interest << ride_name
+      end
+    end
+    attendee_interest
+  end
 end
